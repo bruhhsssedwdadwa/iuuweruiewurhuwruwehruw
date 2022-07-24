eb9ce7070ffaaf33067786c7825c160015255b9d
bruhhsssedwdadwa <108573423+bruhhsssedwdadwa@users.noreply.github.com>
2022-07-24 16:55:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,8 +21,18 @@ var ball = {
     dy:3
 }
 
-function setup(){
+function setup() {
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+  console.log('Model Loaded!');
 }
 
 
