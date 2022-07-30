0ab59f0fdd812f85c6a294dece37b0794532e95c
bruhhsssedwdadwa <108573423+bruhhsssedwdadwa@users.noreply.github.com>
2022-07-30 13:42:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,22 +21,46 @@ var ball = {
     dy:3
 }
 
+rightWristX = 0;
+rightWristY = 0;
+Wristscore = 0;
+
 function setup() {
   var canvas =  createCanvas(700,600);
   canvas.parent('canvas');
   video = createCapture(VIDEO);
   video.size(700,600);
+  video.hide();
   
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
+
 }
 
 function modelLoaded(){
   console.log('Model Loaded!');
 }
 
+function gotPoses(results){
+	if(results.length > 0){
+		console.log(results);
+		rightWristX = results[0].pose.rightWrist.x;
+		rightWristY = results[0].pose.rightWrist.y;
+    Wristscore = results[0].pose.keypoints[10].score;
+    console.log(Wristscore);
+		
+	}
+}
+
 
 function draw(){
+
+  if(Wristscore > 0.2){
+    fill("red");
+    stroke("red");
+    circle(rightWristX, rightWristY, 30);
+    
+  }
 
  background(0); 
 
@@ -173,3 +197,4 @@ function paddleInCanvas(){
     mouseY =0;
   }  
 }
+
